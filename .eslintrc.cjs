b5135154518bdc16f39ac7d3cb5827eac104dfc2
Kaito <74711118+sc30gsw@users.noreply.github.com>
2023-08-29 11:20:55 +0900

    ライブラリの削除

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,8 +20,6 @@ const config = {
       },
     ],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
     'import/first': 'error',
     'import/newline-after-import': 'error',
     'import/no-duplicates': 'error',
